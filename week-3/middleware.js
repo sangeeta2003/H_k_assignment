9a94619926414a6afdf97dc0c5cba87c87a0af3f
sangeeta2003 <mishrasangeeta944@gmail.com>
2025-09-19 18:03:59 +0530

    done middleware

---
+++ week-3/middleware.js
@@ -0,0 +1,9 @@
+// Middlewares
+// You have to create a few standard middlewares for your application.
+// You have to create a middleware for logging the number of requests on a server
+// You have to create a middleware for rate limiting a users request based on their username passed in the header
+// You have to create a middleware for logging the number of errors on a server
+// To test, go to the 01-middlewares folder and run npx jest ./tests
+
+
+const express = require("express");
